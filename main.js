9cfe6b1d0cec853b3d31140f65f13db74a22c886
shuangg.daii <shuangg.daii@gmail.com>
2022-07-26 16:33:21 +0200

    personale website

--- main.js
+++ main.js
@@ -17,6 +17,30 @@ function container() {
 
   window.addEventListener("scroll", container);
 
+
+
+/* animation on scroll of stars block */
+function star() {
+  var stars = document.querySelectorAll(".star");
+
+  for (var i = 0; i < stars.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = stars[i].getBoundingClientRect().top;
+    var elementVisible = 50;
+
+    if (elementTop < windowHeight - elementVisible) {
+      stars[i].classList.add("checked");
+    } else {
+      stars[i].classList.remove("checked");
+    }
+  }
+}
+
+window.addEventListener("scroll", star);
+
+
+
+
 /* animation on scroll of language skill bars */
 function lang() {
     var langs = document.querySelectorAll(".lang");
@@ -35,4 +59,9 @@ function lang() {
   }
   
   window.addEventListener("scroll", lang);  
+
+
+
+
+
   
